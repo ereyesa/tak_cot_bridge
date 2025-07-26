d33091f0bc23568728cd44268ad93bff1b9d39cb
efrain.ra7 <efrain.reyes@innervycs.com>
2025-07-25 22:24:56 -0400

    fixes

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'config/tak_config.yaml']),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', ['config/tak_config.yaml']),
         ('share/' + package_name + '/launch', ['launch/cot_bridge.launch.py']),
     ],
     install_requires=['setuptools', 'pytak'],
