1d5c58d4ea623795c4f709bc2dde29d67d6c46b6
efrain.ra7 <efrain.reyes@innervycs.com>
2025-07-25 23:08:08 -0400

    clean

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'tak_cot_bridge'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=find_packages(where='.', include=['tak_cot_bridge']),
+    packages=find_packages(),  # Busca paquetes automáticamente
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+            [f'resource/{package_name}']),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/tak_config.yaml']),
-        ('share/' + package_name + '/launch', ['launch/cot_bridge.launch.py']),
     ],
-    install_requires=['setuptools', 'pytak'],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Efrain',
-    maintainer_email='efrain.reyes@innervycs.com',
-    description='ROS 2 node to bridge MAVROS telemetry data with FreeTAKServer by generating and sending Cursor-on-Target (CoT) events.',
+    maintainer='Efra',
+    maintainer_email='tu@email.com',
+    description='Bridge from MAVROS GPS data to FreeTAKServer via CoT protocol.',
     license='MIT',
     entry_points={
         'console_scripts': [
